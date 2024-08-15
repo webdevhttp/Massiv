469e26fab6dd539c1e09d55fe300442b22c596f1
Tasha SEii <liamluke2012@icloud.com>
2024-08-15 11:22:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+var colors=['white','black','red'];
+
+var elColor=document.getElementById('colors');
+elColor.textContent=colors[2];
+//dinamiccaly change the color//
+var colors2=['white','black','yellow'];
+
+colors[2]='blue';
+
+var elColor2=document.getElementById('colors2');
+elColor2.textContent=colors[2];
+
+
+
+
+
+
